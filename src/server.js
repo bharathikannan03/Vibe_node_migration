06c7a55de631803815018894c3ce1ecfc2f21b0c
Bharathikannan <bharathikannan@tridentnets.com>
2026-06-19 12:12:50 +0530

    feat: created a create policy api

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import app from "./app.js/index.js";
 
-const port = Number(process.env.PORT || 5000);
+const port = Number(process.env.PORT);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
